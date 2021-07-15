2b0734ad1ca6a10fef5722c0447b988adac1d4c8
haroonbajwa <haroonbajwa99@gmail.com>
2021-07-15 23:29:33 +0500

    First commit

---
+++ client/src/reducers/posts.jsx
@@ -0,0 +1,31 @@
+export default (posts = [], action) => {
+    switch (action.type) {
+        case 'FETCH_ALL':
+            
+            return action.payload;
+            
+        case 'CREATE':
+            
+            return [...posts, action.payload];
+    
+        default:
+            return posts;
+    }
+}
+
+
+// const reducer = (state, action) => {
+//     switch (action.type) {
+//         case 'FETCH_ALL':
+            
+//             return state;
+//         case 'CREATE':
+            
+//             return state;
+    
+//         default:
+//             return state;
+//     }
+// }
+
+//  here we have posts instead of state so we write posts
