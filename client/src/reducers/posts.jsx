cba2a8b0d6857079d11d94824e41393e5c922e9f
haroonbajwa <haroonbajwa99@gmail.com>
2021-07-24 10:51:34 +0500

    action constants & environment variables

--- client/src/reducers/posts.jsx
+++ client/src/reducers/posts.jsx
@@ -1,17 +1,18 @@
+import { CREATE, UPDATE, DELETE, FETCH_ALL } from '../constants/actionTypes';
+
 const posts = (posts = [], action) => {
     switch (action.type) {
         
-        case 'FETCH_ALL':
+        case FETCH_ALL:
             return action.payload;
             
-        case 'CREATE':
+        case CREATE:
             return [...posts, action.payload];
         
-        case 'UPDATE':
-        case 'LIKE':
+        case UPDATE:
             return posts.map( (post) => post._id === action.payload._id ? action.payload : post );
 
-        case 'DELETE':
+        case DELETE:
             return posts.filter((post) => post._id !== action.payload);
     
         default:
--- server/index.js
+++ server/index.js
@@ -2,11 +2,12 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-
+dotenv.config();
 
 
 app.use(bodyParser.json({limit: "30mb", extended:true}));
@@ -15,10 +16,14 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://haroonbajwa99:hbgp9999@cluster0.nw3is.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+    res.send('Hello to Memories API');
+})
+
+
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser : true, useUnifiedTopology : true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser : true, useUnifiedTopology : true})
     .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
