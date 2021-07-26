3fdd861ce93fe04797812e305adff8ce35bb3d8e
haroonbajwa <haroonbajwa99@gmail.com>
2021-07-26 20:20:39 +0500

    google Oauth

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,11 +1,18 @@
 import React, { useState } from 'react'
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core';
+
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+import { GoogleLogin } from 'react-google-login';
+import Icon from './Icon';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import useStyles from './styles';
 import Input from './Input';
 
 const Auth = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    const history = useHistory();
 
     const [showPassword, setShowPassword] = useState(false);
     const [isSignup, setIsSignUp] = useState(false);
@@ -23,6 +30,23 @@ const Auth = () => {
     const switchMode = () => {
         setIsSignUp((prevSetIsSignUp) => !prevSetIsSignUp);
         handleShowPassword(false);
+    }
+
+    const googleSuccess = async (res) => {
+        const result = res?.profileObj;  // (?.) due to this operator it will not throw anny error even it exist
+        const token = res?.tokenId;
+
+        try {
+            dispatch({type: 'AUTH', data: {result, token}});
+            history.push('/');
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const googleFailure = (error) => {
+        console.log(error);
+        console.log("Google Sign In was unsuccessful. Try Again Later");
     }
 
     return (
@@ -48,9 +72,29 @@ const Auth = () => {
                             <Input name="email" label="Email Address" handleChange={handleChange} type="email" />
                             <Input name="password" label="Password" handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} />
                             {isSignup && <Input name='confirmPassword' label="Repeat Password" handleChange={handleChange} type="password" />}
+                            
                             <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit}>
                                 {isSignup ? 'Sign Up' : 'Sign In'}
                             </Button>
+                            <GoogleLogin 
+                                clientId="548405252427-rnlmb7aerftpp3o07nlkr7s086cqshlf.apps.googleusercontent.com"
+                                render={(renderProps) => (
+                                    <Button
+                                        className={classes.googleButton}
+                                        color="primary"
+                                        fullWidth
+                                        onClick={renderProps.onClick}
+                                        disabled={renderProps.disabled}
+                                        startIcon={<Icon />}
+                                        variant="contained"
+                                    >
+                                        Google Sign In
+                                    </Button>
+                                )}
+                                onSuccess={googleSuccess}
+                                onFailure={googleFailure}
+                                cookiePolicy="single_host_origin"
+                            />
                             <Grid container justify="flex-end">
                                 <Grid item>
                                     <Button onClick={switchMode}>
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,14 +1,35 @@
-// import react from 'react';
+import React, { useState, useEffect} from 'react';
+import { useDispatch } from 'react-redux';
 import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 import memories from '../../images/memories.png';
 
 import useStyles from './styles';
+import { LOGOUT } from '../../constants/actionTypes';
 
 const Navbar = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    const history = useHistory();
+    const location = useLocation();
 
-    const user = null;
+    const [user, setUser] = useState(JSON.parse(localStorage.getItem('profile')));
+    console.log(user);
+
+    const logout = () => {
+        dispatch({type: LOGOUT});
+
+        history.push('/');
+        setUser(null);
+    }
+
+    useEffect(() => {
+        const token = user?.token;
+
+        //JWT
+
+        setUser(JSON.parse(localStorage.getItem('profile')));
+    }, [location])
 
     return(
         <div>
@@ -22,7 +43,7 @@ const Navbar = () => {
                         <div className={classes.profile}>
                             <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
                             <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
-                            <Button variant="contained" className={classes.logout} color="secondary">Logout</Button>
+                            <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>Logout</Button>
                         </div>
                     ) : (
                         <Button component={Link} to="/auth" variant="contained" color="primary">Sign In</Button>
