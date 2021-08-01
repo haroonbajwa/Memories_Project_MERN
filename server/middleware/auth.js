053f996f7f64a59e34f1cffb57f542f87e0034aa
haroonbajwa <haroonbajwa99@gmail.com>
2021-08-01 16:27:09 +0500

    post buttons' visibility

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -11,7 +11,7 @@ const auth = async (req, res, next) => {
         let decodedData;
 
         if(token && isCustomAuth) {
-            decodedData = jwt.verify(token, 'text');
+            decodedData = jwt.verify(token, 'test');
             
             req.userId = decodedData?.id;
         } else {
