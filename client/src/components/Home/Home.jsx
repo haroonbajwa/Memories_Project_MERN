d230a65dd4d600fcaf46b324f474e3f86fe3ad1e
haroonbajwa <haroonbajwa99@gmail.com>
2021-08-02 12:54:50 +0500

    pagination & search frontend

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
-import {Container, Grow, Grid} from '@material-ui/core';
+import {Container, Grow, Grid, Paper, AppBar, TextField, Button} from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router';
+import ChipInput from 'material-ui-chip-input';
+
 import { getPosts } from '../../actions/posts';
 import Form from '../Form/Form';
 import Posts from '../Posts/Posts';
-import useStyles from '../../styles';
+import Pagination from '../Pagination';
+// import useStyles from '../../styles';
+import useStyles from './styles';
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 
 const Home = () => {
 
@@ -13,19 +22,69 @@ const Home = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
 
+    const query = useQuery();
+    const history = useHistory();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+
+    const [search, setSearch] = useState('');
+    const [tags, setTags] = useState([]);
+
     useEffect(() => {
         dispatch(getPosts());
     }, [currentId, dispatch]);
 
+    const searchPost = () => {
+        if(search.trim()) {
+            //dispatch => fetch search post
+        } else {
+            history.push('/');
+        }
+    }
+
+    const handleKeyPress = (e) => {
+        if(e.keyCode === 13)
+        {
+            searchPost();
+        }
+    }
+
+    const handleAdd = (tag) => setTags([ ...tags, tag ]);
+    
+    const handleDelete = (tagToDelete) => setTags(tags.filter((tag) => tag !== tagToDelete)) 
+
     return(
             <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={12} md={7}>
+                <Container maxWidth="xl">
+                    <Grid className={classes.gridContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={6} md={9}>
                             <Posts setCurrentId={setCurrentId} />
                         </Grid>
-                        <Grid item xs={12} sm={12} md={4}>
+                        <Grid item xs={12} sm={6} md={3}>
+                            <AppBar className={classes.appBarSearch} position="static" color="inherit">
+                                <TextField 
+                                    name="search" 
+                                    variant="outlined" 
+                                    label="Search Memories"
+                                    onKeyPress={handleKeyPress}
+                                    fullwidth
+                                    value={search}
+                                    onChange = {(e) => setSearch(e.target.value)}
+                                />
+                                <ChipInput 
+                                    style={{margin: '10px 0'}}
+                                    value={tags}
+                                    onAdd={handleAdd}
+                                    onDelete={handleDelete}
+                                    label="Search Tags"
+                                    variant="outlined"
+                                />
+                                <Button onClick={searchPost} className={classes.searchButton} color="primary" variant="contained">Search</Button>
+                            </AppBar>
                             <Form currentId={currentId} setCurrentId={setCurrentId} />
+                            <Paper elevation={6}>
+                                <Pagination />
+                            </Paper>
                         </Grid>
                     </Grid>
                 </Container>
---
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PostDetails = () => {
+    console.log("Post Details");
+    return (
+        <div>
+            POST DETAILS
+        </div>
+    )
+}
+
+export default PostDetails;
